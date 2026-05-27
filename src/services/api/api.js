86f43bcbcff62a05567ae5087ee13565095f3307
poliprene <qualidade@polipreneborracha.com.br>
2026-05-27 16:40:29 -0300

    trocando url do sevidor do backend

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://estudos-r15e.onrender.com'
+  baseURL: 'https://westbackend-zkds.onrender.com'
 });
 
 export default api;
+
+// 'https://westbackend-zkds.onrender.com'
